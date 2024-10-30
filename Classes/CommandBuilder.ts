cb1c46393089e5441c1c23dbea8b94eab99613b7
Char <char@charcodes.online>
2024-10-30 18:44:12 +0000

    Added symbol doc

--- Classes/CommandBuilder.ts
+++ Classes/CommandBuilder.ts
@@ -1,18 +1,39 @@
+/**
+ * Represents the action function that will be executed for a command.
+ * @callback CommandAction
+ * @param {any} message - The message object that triggered the command.
+ * @param {string[]} args - Array of arguments passed to the command.
+ */
 interface CommandAction {
-    (message: any, args: string[]): void;
+  (message: any, args: string[]): void;
+}
+
+/**
+ * Class representing a command with a name, description, and action.
+ * This is used to build and define commands within an application.
+ */
+class CommandBuilder {
+  /** @property {string} name - The name of the command. */
+  name: string;
+
+  /** @property {string} description - A brief description of the command's functionality. */
+  description: string;
+
+  /** @property {CommandAction} action - The function to be executed when the command is called. */
+  action: CommandAction;
+
+  /**
+   * Constructs a new CommandBuilder instance.
+   * @param {Object} config - The configuration object for the command.
+   * @param {string} config.name - The name of the command.
+   * @param {string} config.description - The description of the command.
+   * @param {CommandAction} config.action - The function to execute for the command.
+   */
+  constructor({ name, description, action }: { name: string; description: string; action: CommandAction }) {
+    this.name = name;
+    this.description = description;
+    this.action = action;
   }
-  
-  class CommandBuilder {
-    name: string;
-    description: string;
-    action: CommandAction;
-  
-    constructor({ name, description, action }: { name: string; description: string; action: CommandAction }) {
-      this.name = name;
-      this.description = description;
-      this.action = action;
-    }
-  }
-  
-  export { CommandBuilder };
-  
+}
+
+export { CommandBuilder };
--- Classes/EventBuilder.ts
+++ Classes/EventBuilder.ts
@@ -1,20 +1,43 @@
+/**
+ * Represents the action function that will be executed for an event.
+ * @callback EventAction
+ * @param {...any} args - A variadic list of arguments passed to the event action.
+ */
 interface EventAction {
-    (...args: any[]): void;
+  (...args: any[]): void;
+}
+
+/**
+ * Class representing an event with a name, event type, description, and action.
+ * Used to define and configure events within an application.
+ */
+class EventBuilder {
+  /** @property {string} name - The name of the event handler. */
+  name: string;
+
+  /** @property {string} event - The type or identifier of the event to listen for. */
+  event: string;
+
+  /** @property {string} description - A brief description of the event's purpose or functionality. */
+  description: string;
+
+  /** @property {EventAction} action - The function to execute when the event is triggered. */
+  action: EventAction;
+
+  /**
+   * Constructs a new EventBuilder instance.
+   * @param {Object} config - The configuration object for the event.
+   * @param {string} config.name - The name of the event handler.
+   * @param {string} config.event - The event type or identifier to listen for.
+   * @param {string} config.description - A description of the event's functionality.
+   * @param {EventAction} config.action - The function to execute when the event is triggered.
+   */
+  constructor({ name, event, description, action }: { name: string; event: string; description: string; action: EventAction }) {
+    this.name = name;
+    this.event = event;
+    this.description = description;
+    this.action = action;
   }
-  
-  class EventBuilder {
-    name: string;
-    event: string;
-    description: string;
-    action: EventAction;
-  
-    constructor({ name, event, description, action }: { name: string; event: string; description: string; action: EventAction }) {
-      this.name = name;
-      this.event = event;
-      this.description = description;
-      this.action = action;
-    }
-  }
-  
-  export { EventBuilder };
-  
+}
+
+export { EventBuilder };
